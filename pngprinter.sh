2f16e7aba3b03e6ec0ce5f8a5f9d55b53057ab20
Eridan Domoratskiy <eridan200@mail.ru>
2018-10-26 01:33:31 +0300

    Add chunk reading w/o CRC check

--- pngprinter.sh
+++ pngprinter.sh
@@ -2,40 +2,117 @@
 
 # Functions
 
+## Utilities
+
+# Prints char with specified ASCII code
+#
 # https://unix.stackexchange.com/questions/92447/bash-script-to-get-ascii-values-for-alphabet
+#
+# 1 - int - char code
 chr() {
     [ "$1" -lt 256 ] || return 1
     printf "\\$(printf '%03o' "$1")"
 }
 
+# Applies chr on array and prints
+#
+# @ - int[] - char codes
+function chr_array() {
+    local input=("$@")
+
+    for c in "${input[@]}" ; do
+        chr $c
+    done
+}
+
+# Converts 4 bytes to unsigned integer and prints it
+#
+# @ - int[4] - array of bytes
+function bytes2uint() {
+    local input=("$@")
+
+    local ret=0
+    for ((i=0; $i < 4; ++i)) ; do
+        ret=$(( $ret + (${input[$i]} << (8 * (3 - $i))) ))
+    done
+
+    echo $ret
+}
+
+## PNG
+
 # Checks is bytes is a PNG image
 #
-# 1 - int[] - array of bytes
+# @ - int[8] - array of bytes
 function PNG_check_sign() {
-    sign='137 80 78 71 13 10 26 10'
+    local sign=(137 80 78 71 13 10 26 10)
+    local input=("$@")
 
-    [[ "$(echo ${1:0:${#sign}})" == "$sign" ]] && return 1
+    for ((i=0; $i < ${#sign[@]}; ++i)) ; do
+        [[ ${input[$i]} -ne ${sign[$i]} ]] && return 0
+    done
 
-    return 0
+    return 1
+}
+
+# Reads chunk
+#
+# @ - int[12] - array of bytes
+function PNG_read_chunk() {
+    local input=("$@")
+
+    local length=$(bytes2uint "${input[@]}")
+
+    if [[ $length -ge 4294967296 ]] ; then
+        echo 'Bad chunk size' >&2
+        exit
+    fi
+
+    local type=
+    for b in "${input[@]:4:4}" ; do
+        if (( ($b < 65 || $b > 90) && ($b < 97 || $b > 122) )) ; then
+            echo 'Bad chunk type' >&2
+            exit
+        fi
+
+        type="$type$(chr $b)"
+    done
+
+    input=("${input[@]:8}")
+    local data=("${input[@]:0:$length}")
+
+    # TODO CRC check
+
+    echo "$length $type ${data[@]}"
 }
 
 # Entry point
 
-input="$( xxd -p -c 1 | nawk '
+if ! type nawk &> /dev/null ; then
+    # alias nawk=awk
+
+    function nawk() {
+        awk "$@"
+    }
+fi
+
+input=($( xxd -p -c 1 | nawk '
 BEGIN {
     for (i = 0; i < 256; ++i) {
         hex2dec[sprintf("%02x", i)] = i
     }
 }
+
 {
     print hex2dec[$0]
 }
-')"
+'))
 
-if PNG_check_sign "$input" ; then
+if PNG_check_sign "${input[@]}" ; then
     echo 'File is not PNG image!' >&2
     exit
 fi
 
+input=("${input[@]:8}")
 
-# input="${input:8}"
+PNG_read_chunk "${input[@]}"
